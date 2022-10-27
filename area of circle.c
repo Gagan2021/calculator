a4133a37c3168a7b1561888f57ca85e56344c770
Gagan2021 <79522654+Gagan2021@users.noreply.github.com>
2022-10-27 22:37:12 +0530

    Add files via upload

---
+++ area of circle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+int radius;
+float pie = 3.14;
+
+
+printf("Enter the radius of circle");
+scanf("%d", &radius);
+
+
+printf("the area of cicle is %f", pie*radius*radius);
+return 0;
+
+
+}
